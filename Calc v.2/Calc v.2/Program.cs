7093d783e275c6247ad47a98ba6582d7156f4e17
Vladimirzhil <105555106+Vladimirzhil@users.noreply.github.com>
2022-11-16 16:58:40 +0300

    Add files via upload

---
+++ Calc v.2/Calc v.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+
+namespace Calc_v._2
+{
+    class Program
+    {
+   
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string expression;
+                Console.WriteLine("Введите выражение: ");
+                expression = Console.ReadLine();
+                Console.WriteLine(Calculate(expression));
+            }
+        }
+        public static double Calculate(string expression)
+        {         
+            DataTable table = new DataTable();    
+            table.Columns.Add("expression", typeof(string), expression);        
+            DataRow row = table.NewRow();         
+            table.Rows.Add(row);
+            return double.Parse((string)row["expression"]);
+        } 
+    }   
+} 
