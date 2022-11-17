09947f9ccc97b7f7d3e8bd90e76899088aba55ec
Vladimirzhil <105555106+Vladimirzhil@users.noreply.github.com>
2022-11-17 11:02:02 +0300

    Update MainController.cs

--- Lab3v2/Lab3v2/Controllers/MainController.cs
+++ Lab3v2/Lab3v2/Controllers/MainController.cs
@@ -5,18 +5,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Lab3v2.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/DataBase")]
     [ApiController]
     public class MainController : ControllerBase
     {
-        private readonly TypeofworkContext _сontext;
+        private readonly Context _сontext;
 
 
-        public MainController(TypeofworkContext сontext)
+        public MainController(Context сontext)
         {
             _сontext = сontext;
         }
-        [HttpGet]
+        [HttpGet("GetFull")]
         public async Task<ActionResult<IEnumerable<Typeofwork>>> GetTypeofworks()
         {
             if (_сontext.Typeofworks == null)
@@ -27,7 +27,7 @@ namespace Lab3v2.Controllers
         }
 
 
-        [HttpGet("{id}")]
+        [HttpGet("Typeofwork/{id}")]
         public async Task<ActionResult<Typeofwork>> GetTypeofwork(int id)
         {
             if (_сontext.Typeofworks == null)
@@ -42,7 +42,22 @@ namespace Lab3v2.Controllers
 
             return typeofwork;
         }
-        [HttpPost]
+        [HttpGet("Workcatalog/{id}")]
+        public async Task<ActionResult<Workcatalog>> GetWorkcatalog(int id)
+        {
+            if (_сontext.Workcatalogs == null)
+            {
+                return NotFound();
+            }
+            var workcatalog = await _сontext.Workcatalogs.FindAsync(id);
+            if (workcatalog == null)
+            {
+                return NotFound();
+            }
+
+            return workcatalog;
+        }
+        [HttpPost("Typeofwork")]
         public async Task<ActionResult<Typeofwork>> PostTypeofwork(Typeofwork typeofwork)
         {
             _сontext.Typeofworks.Add(typeofwork);
@@ -50,32 +65,34 @@ namespace Lab3v2.Controllers
 
             return CreatedAtAction(nameof(GetTypeofwork), new { id = typeofwork.Id }, typeofwork);
         }
-        [HttpPut("{id}")]
+        [HttpPost("Workcatalog")]
+        public async Task<ActionResult<Workcatalog>> PostWorkcatalog(Workcatalog workcatalog)
+        {
+            _сontext.Workcatalogs.Add(workcatalog);
+            await _сontext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTypeofwork), new { id = workcatalog.Id }, workcatalog);
+        }
+        [HttpPut("Typeofwork/{id}")]
         public async Task<IActionResult> PutTypeofwork(int id, Typeofwork typeofwork)
         {
             if (id != typeofwork.Id)
             {
                 return BadRequest();
             }
-            _сontext.Entry(typeofwork).State = EntityState.Modified;
-            try
+            return NoContent();
+        }
+        [HttpPut("Workcatalog/{id}")]
+        public async Task<IActionResult> PutWorkcatalog(int id, Workcatalog workcatalog)
+        {
+            if (id != workcatalog.Id)
             {
-                await _сontext.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TypeofworkExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                return BadRequest();
             }
             return NoContent();
         }
-        [HttpDelete("{id}")]
+
+        [HttpDelete("Typeofwork/{id}")]
         public async Task<IActionResult> DeleteTypeofwork(int id)
         {
             if (_сontext.Typeofworks == null)
@@ -93,10 +110,23 @@ namespace Lab3v2.Controllers
 
             return NoContent();
         }
-        private bool TypeofworkExists(long id)
+        [HttpDelete("Workcatalog/{id}")]
+        public async Task<IActionResult> DeleteWorkcatalog(int id)
         {
-            return (_сontext.Typeofworks?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
+            if (_сontext.Workcatalogs == null)
+            {
+                return NotFound();
+            }
+            var workcatalog = await _сontext.Workcatalogs.FindAsync(id);
+            if (workcatalog == null)
+            {
+                return NotFound();
+            }
 
+            _сontext.Workcatalogs.Remove(workcatalog);
+            await _сontext.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
