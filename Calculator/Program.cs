f6ed5c285c341db73f08141b4ba097e776f52aae
Vladimirzhil <105555106+Vladimirzhil@users.noreply.github.com>
2022-11-17 12:37:20 +0300

    Update Program.cs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -9,25 +9,26 @@ namespace CALC
     {
         static void Main(string[] args)
         {
-            while (true) 
+            while (true)
             {
-                Console.Write("Введите выражение: "); 
-                Console.WriteLine(RPN.Calculate(Console.ReadLine())); 
+                Console.Write("Введите выражение: ");
+                Console.WriteLine(RPN.Calculate(Console.ReadLine()));
             }
         }
     }
     class RPN
     {
-        static private bool IsDelimeter(char c)
+        private static  bool IsDelimeter(char c)
         {
-            return " =".IndexOf(c) != -1
-                
+            return " =".IndexOf(c) != -1;
+
+
         }
-        static private bool IsOperator(char с)
+        private static  bool IsOperator(char с)
         {
-            if "+-/*()".IndexOf(с) != -1   
+            return "+-/*()".IndexOf(с) != -1;
         }
-        static private byte GetPriority(char s)
+        private static  byte GetPriority(char s)
         {
             switch (s)
             {
@@ -40,91 +41,119 @@ namespace CALC
                 default: return 4;
             }
         }
-        static public double Calculate(string input)
+        public static  double Calculate(string input)
         {
-            string output = ConvertExpression(input); 
-            double result = ExpressionCounting(output); 
-            return result; 
+            string output = ConvertExpression(input);
+            double result = ExpressionCounting(output);
+            return result;
         }
-        static private string ConvertExpression(string input)
+        private static string ConvertExpression(string input)
         {
             string output = string.Empty;
-            Stack<char> operStack = new Stack<char>(); 
+            Stack<char> operStack = new Stack<char>();
 
-            for (int i = 0; i < input.Length; i++) 
+            for (int i = 0; i < input.Length; i++)
             {
-                if (Char.IsDigit(input[i])) 
-                {
-                    while (!IsDelimeter(input[i]) && !IsOperator(input[i]))
-                    {
-                        output += input[i]; 
-                        i++; 
-                        if (i == input.Length) break; 
-                    }
-
-                    output += " "; 
-                    i--; 
-                }
-                if (IsOperator(input[i]))
-                {
-                    if (input[i] == '(')
-                        operStack.Push(input[i]); 
-                    else if (input[i] == ')') 
-                    {            
-                        char s = operStack.Pop();
-                        while (s != '(')
-                        {
-                            output += s.ToString() + ' ';
-                            s = operStack.Pop();
-                        }
-                    }
-                    else 
-                    {
-                        if (operStack.Count > 0 && GetPriority(input[i]) <= GetPriority(operStack.Peek())) 
-                            output += operStack.Pop().ToString() + " ";
-                        operStack.Push(char.Parse(input[i].ToString())); 
-                    }
-                }
-            }
-            
-            while (operStack.Count > 0)
-                output += operStack.Pop() + " ";
-            Console.WriteLine("Постфиксная запись: " + output);
-            return output; 
-        }
-        static private double ExpressionCounting(string input)
-        {
-            double result = 0; 
-            Stack<double> temp = new Stack<double>(); 
-            for (int i = 0; i < input.Length; i++) 
-            { 
                 if (Char.IsDigit(input[i]))
                 {
-                    string a = string.Empty;
-                    while (!IsDelimeter(input[i]) && !IsOperator(input[i])) 
+                    output += Checknumber(input, i);
+                    while (!IsDelimeter(input[i]) && !IsOperator(input[i]))
                     {
-                        a += input[i]; 
                         i++;
                         if (i == input.Length) break;
                     }
-                    temp.Push(double.Parse(a));
+                    output += " ";
                     i--;
                 }
-                else if (IsOperator(input[i])) 
+                if (IsOperator(input[i]))
                 {
-                    double a = temp.Pop();
-                    double b = temp.Pop();
-                    switch (input[i]) 
-                    {
-                        case '+': result = b + a; break;
-                        case '-': result = b - a; break;
-                        case '*': result = b * a; break;
-                        case '/': result = b / a; break;
-                    }
-                    temp.Push(result); 
+                    output += CheckOpertor(input, i, output, operStack);
                 }
             }
-            return temp.Peek(); 
+
+            while (operStack.Count > 0)
+                output += operStack.Pop() + " ";
+            return output;
         }
+
+
+        private static double ExpressionCounting(string input)
+        {
+            
+            {
+                double result = 0;
+                Stack<double> temp = new Stack<double>();
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (Char.IsDigit(input[i]))
+                    {
+                        string a = string.Empty;
+                        while (!IsDelimeter(input[i]) && !IsOperator(input[i]))
+                        {
+                            a += input[i];
+                            i++;
+                            if (i == input.Length) break;
+                        }
+                        temp.Push(double.Parse(a));
+                        i--;
+                    }
+                    else if (IsOperator(input[i]))
+                    {
+                        double a = temp.Pop();
+                        double b = temp.Pop();
+                        switch (input[i])
+                        {
+                            case '+': result = b + a; break;
+                            case '-': result = b - a; break;
+                            case '*': result = b * a; break;
+                            case '/': result = b / a; break;
+                        }
+                        temp.Push(result);
+                    }
+                }
+                return temp.Peek();
+            }
+
+        }
+
+        public static string Checknumber(string input, int i)
+        {
+            string output = string.Empty;
+            while (!IsDelimeter(input[i]) && !IsOperator(input[i]))
+            {
+                output += input[i];
+                i++;
+                if (i == input.Length) break;
+            }
+
+            return output;
+        }
+        public static string CheckOpertor(string input, int i, string output, Stack<char> operStack)
+        {
+
+            if (input[i] == '(')
+                operStack.Push(input[i]);
+            else if (input[i] == ')')
+            {
+                char s = operStack.Pop();
+                while (s != '(')
+                {
+                    output += s.ToString() + ' ';
+                    s = operStack.Pop();
+                }
+            }
+            else
+            {
+                if (operStack.Count > 0 && GetPriority(input[i]) <= GetPriority(operStack.Peek()))
+                    output += operStack.Pop().ToString() + " ";
+                operStack.Push(char.Parse(input[i].ToString()));
+            }
+            return output;
+        }
+       
+        }
+
+    }
+
     }
 }
