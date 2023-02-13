9b9d5e3666a1f30119fb9ea7a46d6caf50065864
Vladimirzhil <105555106+Vladimirzhil@users.noreply.github.com>
2023-02-13 18:44:17 +0300

    Add files via upload

---
+++ Interior decorating company/Interior decorating company/DAL/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DAL
+{
+    public class Employee
+    {
+        [Key]
+        public int employee_id { get; set; }
+        public string Employees_last_name { get; set; }
+        public int JobtitleId { get; set; }
+        public Jobtitle? Jobtitle { get; set; }
+        public LinkedList<Designproject> Designprojects { get; set; } = new();
+    }
+}
---
+++ Interior decorating company/Interior decorating company/DAL/Typeofworks.cs
@@ -0,0 +1,12 @@
+﻿namespace DAL
+{
+    public class Typeofwork
+    {
+        public int Order_number { get; set; }
+        public int Job_line_number { get; set; }
+        public string? Value { get; set; }
+        public int Job_id { get; set; }
+        public Workcatalog? Workcatalog { get; set; }
+        public Orderforobject? Orderforobject { get; set; }
+    }
+}
